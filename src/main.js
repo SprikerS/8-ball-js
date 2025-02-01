4ff8f97ca85e02ff65aef3f3dcc9252fe4f3e29d
SprikerS <jeremyestelo17@gmail.com>
2025-01-31 20:10:53 -0500

    refactor: wrap global shortcut registration in an async IIFE

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import { register } from '@tauri-apps/plugin-global-shortcut';
 let isClickeable = true;
 const window = getCurrentWindow();
 
-await register('v', () => {
-  isClickeable = !isClickeable;
-  window.setIgnoreCursorEvents(isClickeable);
-});
+(async () => {
+  await register('v', () => {
+    isClickeable = !isClickeable;
+    window.setIgnoreCursorEvents(isClickeable);
+  });
+})();
